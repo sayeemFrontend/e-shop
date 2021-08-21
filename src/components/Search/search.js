aa451b7d0dc2c6a2f68268fe1040e5f71ae420bb
Sayeem-Lotus-Technology <sayeemfrontend@gmail.com>
2021-08-21 18:02:39 +0600

    yaaa

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import classes from './search.module.css'
+import search from "./../../assets/Icon/search.svg"
 const Search = ({ placeholder, icon }) => {
    return (
       <div className={classes.searchBar + " d-flex align-items-center"}>
          <input type="text" placeholder={placeholder && placeholder} />
-         {icon && <i class="fas fa-search fa-xs"></i>}
+         {icon && <img src={search} alt="search" />}
       </div>
    )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import "@fortawesome/fontawesome-free/js/brands.js"
-import "@fortawesome/fontawesome-free/js/solid.js"
-import "@fortawesome/fontawesome-free/js/fontawesome.js"
 import "bootstrap/dist/css/bootstrap-grid.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.js"
 import React from 'react';
