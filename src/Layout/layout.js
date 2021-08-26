84ec0b0c9b74017daa498a999844d57d5f3b4e4c
Sayeem-Lotus-Technology <sayeemfrontend@gmail.com>
2021-08-26 15:03:43 +0600

    check

--- src/Layout/layout.js
+++ src/Layout/layout.js
@@ -1,13 +1,21 @@
 import { Route, Switch } from "react-router-dom";
+import SimpleSlider from "../components/Sliders/ReactSlick/slider";
 import Footer from "../parts/Footer/footer";
 import Header from './../parts/Header/header';
 import ListView from './../parts/ListView/listView';
+import Slider from "../components/Sliders/SliderOne/Slider"
+// import Slider from "../components/Sliders/CustomSlider/slider";
 
 const Layout = () => {
     return (
         <div>
 
             <Header />
+            <div>
+                {/* <Slider /> */}
+            </div>
+            {/* <Slider /> */}
+            <SimpleSlider />
 
             <Switch>
                 {/* <Route path="/list" ><ListView /></Route> */}
